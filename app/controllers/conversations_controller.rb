dc196838f4b88d06ab6e93d88bdd4f31ba300ca5
davidclaveau <daveclaveau@gmail.com>
2021-07-22 12:39:15 -0700

    Add conversations controller

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,13 @@
+class ConversationsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'conversations/index'
+  get 'conversations/create'
+  get 'conversations/show'
+  get 'conversations/destroy'
   root 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
