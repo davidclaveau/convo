8f7a9d678ebf8ccdf5abb5ed47b6d55fef73bd3e
davidclaveau <daveclaveau@gmail.com>
2021-07-22 12:55:24 -0700

    Add conversations controller and routes

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,13 +1,38 @@
 class ConversationsController < ApplicationController
   def index
+    conversation = Conversation.all.order(created_at: :desc)
+    render json: conversation
   end
 
   def create
+    conversation = Conversation.create!(conversation_params)
+    if conversation
+      render json: conversation
+    else
+      render json: conversation.errors
+    end
   end
 
   def show
+    if conversation
+      render json: conversation
+    else
+      render json: conversation.errors
+    end
   end
 
   def destroy
+    conversation&.destroy
+    render json: { message: 'Conversation deleted!' }
+  end
+
+  private
+
+  def conversation_params
+    params.permit(:name, :username, :password)
+  end
+
+  def conversation
+    @conversation ||= Conversation.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get 'conversations/index'
-  get 'conversations/create'
-  get 'conversations/show'
-  get 'conversations/destroy'
+  post 'conversations/create'
+  get '/show/:id', to: 'conversations#show'
+  delete '/destroy/:id', to: 'conversations#delete'
+  
   root 'homepage#index'
+  get '/*path' => 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
